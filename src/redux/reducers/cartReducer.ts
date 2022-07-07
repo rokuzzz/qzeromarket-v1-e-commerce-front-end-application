d1261f064c3e5045e406682e39a4b46579ce19d7
Roman Kuzero <rombeldan@gmail.com>
2022-07-07 15:46:47 +0300

    create addToCart reducer in cartReducers.ts

--- src/redux/reducers/cartReducer.ts
+++ src/redux/reducers/cartReducer.ts
@@ -1,14 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { ProductInCart } from "../../types/cart";
+import { ProductInCart } from './../../types/cart';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Cart } from "../../types/cart";
 
-const initialState: ProductInCart[] = []
+const initialState: Cart = {
+  products: [],
+  total: 0
+}
 
 const cartSlice = createSlice({
   name: 'cart reducer',
   initialState: initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-
+  reducers: {
+    addToCart: (state, action: PayloadAction<ProductInCart>) => {
+      state.products.push(action.payload)
+      state.total += 1
+    },
+    // increaseProduct: (state, action: PayloadAction<string>) => {
+    //   state.products.map(
+    //     product => {
+    //       if()
+    //     })
+    // }
   }
 })
 
